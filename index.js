acd186beba8c7ac9a9bdabc55bdf193a9ee50dda
JacobLayton <jlayjlay3@gmail.com>
2019-12-10 23:35:54 -0800

    Post and Put requests complete

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const db = require("./data/db.js");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send("Server running for node-api2-project");
 });
@@ -33,6 +35,36 @@ server.get("/api/posts/:id", (req, res) => {
       res.status(500).json(error);
     });
 });
+
+server.post("/api/posts", (req, res) => {
+  const postInfo = req.body;
+  console.log(postInfo);
+
+  db.insert(postInfo)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const postInfo = req.body;
+
+  db.update(id, postInfo)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json(updated);
+      } else {
+        res.status(404).json({ message: "Cannot update non-existent post" })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
 
 const port = 5001;
 server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`));
