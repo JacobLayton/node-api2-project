c7a7a2b5e37ff9b6a8e14017dd9145936b766d9e
JacobLayton <jlayjlay3@gmail.com>
2019-12-11 00:03:59 -0800

    Get comments and Post new comment complete

--- index.js
+++ index.js
@@ -66,5 +66,55 @@ server.put("/api/posts/:id", (req, res) => {
     });
 })
 
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(200).json({ message: `Post has been deleted` }); // .end sends response back to client to end process
+      } else {
+        res.status(404).json({ message: "That post does not exist" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get("/api/posts/:id/comments", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(404).json({ message: "The post with the specified ID does not exist" })
+  }
+  db.findPostComments(id)
+    .then(comments => {
+      res.status(200).json(comments);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+});
+
+server.post("/api/posts/:id/comments", (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(404).json({ message: "The post with the specified ID does not exist" })
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "Please prodive text for the comment" })
+  } else {
+    db.insertComment(req.body)
+      .then(comment => {
+        res.status(201).json(comment);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  }
+})
+
+
 const port = 5001;
 server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`));
